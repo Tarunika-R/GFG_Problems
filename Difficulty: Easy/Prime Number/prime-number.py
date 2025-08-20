1d1f9e0b68b032669bfd5a209958e5fc22a589ba
Tarunika R <127477721+Tarunika-R@users.noreply.github.com>
2025-08-20 20:04:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        elif n <= 3:
+            return True
+        elif n % 2 == 0 or n % 3 == 0:
+            return False
+        
+        for i in range(5, int(math.sqrt(n)) + 1, 6):
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+        return True
+        
